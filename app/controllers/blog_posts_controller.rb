aec6eb43ac328d6352c2eb8a2c55d2b781db0374
Salwa99 <s.ritage99@gmail.com>
2023-11-15 17:26:50 +0100

    Add blog post show action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,10 @@ class BlogPostsController < ApplicationController
     def index
         @blog_posts = PostModel.all
     end
+
+    def show
+        @blog_post = PostModel.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
 end
